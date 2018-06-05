4feac9e032f2c95020c02bd796a21a085dd7d604
zalazni <37899981+zalazni@users.noreply.github.com>
2018-06-05 21:10:47 +0200

    Update SQL_poizvedbe.sql

--- flask/SQL_poizvedbe.sql
+++ flask/SQL_poizvedbe.sql
@@ -10,8 +10,10 @@ WHERE tocke_plovi.plov_idplov = 1
 ORDER BY tocke;
 
 -- treba je to spodaj updateat z novimi tabelami za plove, ki imajo klub!
-CREATE VIEW delni1 AS
-SELECT rezultati_nikola_1plov.sailno, rezultati_nikola_1plov.ime, rezultati_nikola_1plov.spol, rezultati_nikola_1plov.leto_rojstva, rezultati_nikola_1plov.tocke_plov AS prvi, rezultati_nikola_2plov.tocke_plov AS drugi, rezultati_nikola_3plov.tocke_plov AS tretji, rezultati_nikola_4plov.tocke_plov AS četrti from rezultati_nikola_1plov
+-- delni2 je view z dodanim stolpcem klubov
+CREATE VIEW delni2 AS
+SELECT rezultati_nikola_1plov.sailno, rezultati_nikola_1plov.ime, rezultati_nikola_1plov.spol, rezultati_nikola_1plov.leto_rojstva, rezultati_nikola_1plov.tocke_plov AS prvi, rezultati_nikola_2plov.tocke_plov AS drugi, rezultati_nikola_3plov.tocke_plov AS tretji, rezultati_nikola_4plov.tocke_plov AS četrti, test.ime_kluba from rezultati_nikola_1plov
+JOIN test ON test.sailno = rezultati_nikola_1plov.sailno
 JOIN rezultati_nikola_2plov ON rezultati_nikola_1plov.sailno = rezultati_nikola_2plov.sailno
 JOIN rezultati_nikola_3plov ON rezultati_nikola_1plov.sailno = rezultati_nikola_3plov.sailno
 JOIN rezultati_nikola_4plov ON rezultati_nikola_1plov.sailno = rezultati_nikola_4plov.sailno;
@@ -19,5 +21,4 @@ JOIN rezultati_nikola_4plov ON rezultati_nikola_1plov.sailno = rezultati_nikola_
 -- za generacijo rezultatov, ^\\d+$ = :
 SELECT *, (CASE WHEN prvi~E'^\\d+$' THEN prvi::integer ELSE 104 END + CASE WHEN drugi~E'^\\d+$' THEN drugi::integer ELSE 104 END + CASE WHEN tretji~E'^\\d+$' THEN tretji::integer ELSE 104 END + CASE WHEN četrti~E'^\\d+$' THEN četrti::integer ELSE 104 END) - greatest(CASE WHEN prvi~E'^\\d+$' THEN prvi::integer ELSE 104 END, CASE WHEN drugi~E'^\\d+$' THEN drugi::integer ELSE 104 END, CASE WHEN tretji~E'^\\d+$' THEN tretji::integer ELSE 104 END, CASE WHEN četrti~E'^\\d+$' THEN četrti::integer ELSE 104 END) AS net, (CASE WHEN prvi~E'^\\d+$' THEN prvi::integer ELSE 104 END + CASE WHEN drugi~E'^\\d+$' THEN drugi::integer ELSE 104 END + CASE WHEN tretji~E'^\\d+$' THEN tretji::integer ELSE 104 END + CASE WHEN četrti~E'^\\d+$' THEN četrti::integer ELSE 104 END) AS tot FROM delni1
 ORDER BY net ASC, tot ASC; 
-
 
